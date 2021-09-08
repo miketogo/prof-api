6bcc5b2f8f7a35cddac2dc553ca18309ee6b3fa9
Mikhail Surikov <miketogo@yandex.ru>
2021-09-08 22:15:03 +0300

    fix: Вернул даты подачи счетчиков

--- controllers/users.js
+++ controllers/users.js
@@ -193,7 +193,7 @@ module.exports.updateMeterReadings = (req, res, next) => {
       let date = moment(realDate.toISOString()).tz("Europe/Moscow").format('D.MM.YYYY  HH:mm')
       const { hotWater, coldWater } = req.body;
       if (user.meterReadings.length === 0) {
-        if (Number(date.split('.')[0]) >= 1 && Number(date.split('.')[0]) <= 25) {
+        if (Number(date.split('.')[0]) >= 20 && Number(date.split('.')[0]) <= 25) {
           let meterReadings = {
             time: date,
             hotWaterSupply: hotWater,
@@ -237,7 +237,7 @@ module.exports.updateMeterReadings = (req, res, next) => {
         }
 
       } else {
-        if (Number(date.split('.')[0]) >= 1 && Number(date.split('.')[0]) <= 25) {
+        if (Number(date.split('.')[0]) >= 20 && Number(date.split('.')[0]) <= 25) {
           if (Number(user.meterReadings[user.meterReadings.length - 1].time.split('.')[1]) !== Number(date.split('.')[1])) {
             if (Number(user.meterReadings[user.meterReadings.length - 1].hotWaterSupply) <= hotWater && Number(user.meterReadings[user.meterReadings.length - 1].coldWaterSupply) <= coldWater) {
               let meterReadings = {
